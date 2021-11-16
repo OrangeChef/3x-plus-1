5518740fa81bbdcc78d274ddd8ea9393e6761c76
OrangeChef <redyellowchef@gmail.com>
2021-11-16 15:11:56 -0500

    Interval settings and export button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,11 @@ namespace _3x_1
             this.GoButton = new System.Windows.Forms.Button();
             this.NumberUpDown = new System.Windows.Forms.NumericUpDown();
             this.CancelButton = new System.Windows.Forms.Button();
+            this.IntervalUpDown = new System.Windows.Forms.NumericUpDown();
+            this.NumbersLabel = new System.Windows.Forms.Label();
+            this.ExportButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.NumberUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IntervalUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // NumbersBox
@@ -44,13 +48,14 @@ namespace _3x_1
             this.NumbersBox.Name = "NumbersBox";
             this.NumbersBox.Size = new System.Drawing.Size(676, 424);
             this.NumbersBox.TabIndex = 0;
+            this.NumbersBox.TabStop = false;
             // 
             // GoButton
             // 
-            this.GoButton.Location = new System.Drawing.Point(695, 42);
+            this.GoButton.Location = new System.Drawing.Point(694, 71);
             this.GoButton.Name = "GoButton";
             this.GoButton.Size = new System.Drawing.Size(93, 23);
-            this.GoButton.TabIndex = 1;
+            this.GoButton.TabIndex = 2;
             this.GoButton.Text = "Go!";
             this.GoButton.UseVisualStyleBackColor = true;
             this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
@@ -59,18 +64,18 @@ namespace _3x_1
             // 
             this.NumberUpDown.Location = new System.Drawing.Point(695, 13);
             this.NumberUpDown.Maximum = new decimal(new int[] {
-            65536,
+            1000000000,
             0,
             0,
             0});
             this.NumberUpDown.Name = "NumberUpDown";
             this.NumberUpDown.Size = new System.Drawing.Size(92, 23);
-            this.NumberUpDown.TabIndex = 2;
+            this.NumberUpDown.TabIndex = 0;
             this.NumberUpDown.ValueChanged += new System.EventHandler(this.NumberUpDown_ValueChanged);
             // 
             // CancelButton
             // 
-            this.CancelButton.Location = new System.Drawing.Point(694, 71);
+            this.CancelButton.Location = new System.Drawing.Point(693, 100);
             this.CancelButton.Name = "CancelButton";
             this.CancelButton.Size = new System.Drawing.Size(93, 23);
             this.CancelButton.TabIndex = 3;
@@ -78,11 +83,51 @@ namespace _3x_1
             this.CancelButton.UseVisualStyleBackColor = true;
             this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
             // 
+            // IntervalUpDown
+            // 
+            this.IntervalUpDown.Location = new System.Drawing.Point(694, 42);
+            this.IntervalUpDown.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.IntervalUpDown.Name = "IntervalUpDown";
+            this.IntervalUpDown.Size = new System.Drawing.Size(92, 23);
+            this.IntervalUpDown.TabIndex = 1;
+            this.IntervalUpDown.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.IntervalUpDown.ValueChanged += new System.EventHandler(this.IntervalUpDown_ValueChanged);
+            // 
+            // NumbersLabel
+            // 
+            this.NumbersLabel.AutoSize = true;
+            this.NumbersLabel.Location = new System.Drawing.Point(696, 130);
+            this.NumbersLabel.Name = "NumbersLabel";
+            this.NumbersLabel.Size = new System.Drawing.Size(13, 15);
+            this.NumbersLabel.TabIndex = 4;
+            this.NumbersLabel.Text = "0";
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Location = new System.Drawing.Point(696, 413);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(92, 23);
+            this.ExportButton.TabIndex = 5;
+            this.ExportButton.Text = "Export";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            this.ExportButton.Click += new System.EventHandler(this.ExportButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ExportButton);
+            this.Controls.Add(this.NumbersLabel);
+            this.Controls.Add(this.IntervalUpDown);
             this.Controls.Add(this.CancelButton);
             this.Controls.Add(this.NumberUpDown);
             this.Controls.Add(this.GoButton);
@@ -91,7 +136,9 @@ namespace _3x_1
             this.Text = "3x + 1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.NumberUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IntervalUpDown)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -101,6 +148,9 @@ namespace _3x_1
         private System.Windows.Forms.Button GoButton;
         private System.Windows.Forms.NumericUpDown NumberUpDown;
         private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.NumericUpDown IntervalUpDown;
+        private System.Windows.Forms.Label NumbersLabel;
+        private System.Windows.Forms.Button ExportButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
@@ -15,6 +16,9 @@ namespace _3x_1
     {
 
         private int num;
+        private int interval;
+        private int iterations;
+        private int initial;
         private bool calculate = false;
 
         public Form1()
@@ -25,6 +29,9 @@ namespace _3x_1
         private void Form1_Load(object sender, EventArgs e)
         {
             Reset();
+
+            new ToolTip().SetToolTip(NumberUpDown, "The number you want to use for calculations.");
+            new ToolTip().SetToolTip(IntervalUpDown, "Time time in ms to wait before the next calculation.");
         }
 
         private void NumberUpDown_ValueChanged(object sender, EventArgs e)
@@ -32,12 +39,22 @@ namespace _3x_1
             GoButton.Enabled = NumberUpDown.Value > 0;
         }
 
+        private void IntervalUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            interval = (int)IntervalUpDown.Value;
+        }
+
         private void GoButton_Click(object sender, EventArgs e)
         {
             GoButton.Enabled = false;
-            num = (int)NumberUpDown.Value;
+            CancelButton.Enabled = true;
             calculate = true;
 
+            num = (int)NumberUpDown.Value;
+            initial = num;
+            iterations = 0;
+
+            NumbersLabel.Text = iterations.ToString();
             NumbersBox.Items.Clear();
 
             Calculate();
@@ -47,6 +64,8 @@ namespace _3x_1
         {
             if (!calculate)
                 return;
+
+            num = Math.Abs(num);
 
             bool even = num % 2 == 0;
 
@@ -60,12 +79,16 @@ namespace _3x_1
                 num++;
             }
 
+            num = Math.Abs(num);
+
+            iterations++;
             NumbersBox.Items.Add(num.ToString());
             NumbersBox.SelectedIndex = NumbersBox.Items.Count - 1;
+            NumbersLabel.Text = iterations.ToString();
 
             if (num != (int)NumberUpDown.Value && num != 1)
             {
-                Wait(10);
+                Wait(interval);
                 Calculate();
                 return;
             }
@@ -73,6 +96,7 @@ namespace _3x_1
             calculate = false;
             NumberUpDown.Enabled = true;
             GoButton.Enabled = true;
+            ExportButton.Enabled = true;
 
             MessageBox.Show("Finished!", "3x + 1");
         }
@@ -82,6 +106,12 @@ namespace _3x_1
             calculate = false;
             NumberUpDown.Enabled = true;
             GoButton.Enabled = false;
+            ExportButton.Enabled = false;
+            CancelButton.Enabled = false;
+
+            iterations = 0;
+
+            NumbersLabel.Text = iterations.ToString();
         }
 
         private void Wait(int milliseconds)
@@ -108,6 +138,28 @@ namespace _3x_1
         private void CancelButton_Click(object sender, EventArgs e)
         {
             Reset();
+        }
+
+        private void ExportButton_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists("./Exports"))
+                Directory.CreateDirectory("./Exports");
+
+            List<string> numbers = new List<string>();
+            for (int i = 0; i < NumbersBox.Items.Count; i++)
+            {
+                numbers.Add(NumbersBox.Items[i].ToString());
+            }
+
+            try
+            {
+                File.WriteAllLines($"./Exports/{initial}.txt", numbers.ToArray());
+                MessageBox.Show($"Calculations exported as \"{initial}.txt\" to \"./Exports\" successfully.", "3x + 1");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
         }
     }
 }
